f7738a19cd41ab00f0a6eb86e0adbd987568d793
Kamran <bytalibli2@gmail.com>
2023-05-15 14:09:35 +0400

    Social Media, Settings, User Logs m,f,s create

---
+++ database/migrations/2023_05_15_094623_create_settings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string("logo_image");
+            $table->string("home_slider_image");
+            $table->string("adverse_image")->nullable();
+            $table->string("footer_text")->nullable();
+            $table->string("footer_description")->nullable();
+            $table->boolean("most_popular_status")->default(0);
+            $table->boolean("recent_article_status")->default(0);
+            $table->boolean("search_status")->default(0);
+            $table->string("home_pages_slider_text_1")->nullable();
+            $table->string("home_pages_slider_text_2")->nullable();
+            $table->string("home_pages_slider_text_3")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
